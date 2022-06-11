3d08d05021eada4429038d25946b9f3cd3579cba
Wocx-32 <63423836+Wocx-32@users.noreply.github.com>
2022-06-11 05:15:11 +0500

    more progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pickle
 import time
+from io import StringIO
+from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,13 +14,13 @@ import config
 driver_path = './geckodriver'
 url = 'https://empower.fccollege.edu.pk/fusebox.cfm'
 
-def get_source(url) -> str:
+def register(url, term, courses) -> str:
     options = Options()
     # options.headless = True
 
     
     driver = webdriver.Firefox(options=options, executable_path=driver_path)
-    
+
     driver.get(url)
 
     username = driver.find_element(By.XPATH, '//*[@id="empower_usrn"]')
@@ -42,38 +44,62 @@ def get_source(url) -> str:
 
     drop = Select(catalog)
 
-    drop.select_by_value('202122')
+    drop.select_by_value(term)
 
     driver.find_element(By.NAME, 'GetWindow').click()
 
     WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.NAME, 'add'))).click()
     WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'button'))).click()
 
-    form = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.NAME, 'form1')))
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.NAME, 'form1')))
     # count = 0
+    
+    registered = []
 
-    for i in form.find_elements(By.TAG_NAME, 'tr'):
-        children = i.find_elements(By.TAG_NAME, 'td')
-        if len(children) >= 36 and children[2].text == 'Credit':
-            print(len(children))
-            print([child.text for child in children])
-            # print(children[5].text, children[6].text, children[7].text)
-            print('\n')
-            # count += 1
-        # children = i.find_elements(By.TAG_NAME, 'td')
-        # if len(children) > 17:
-        #     print(children[5].text, children[6].text, children[7].text)
-            
+    source = driver.page_source
+    
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(StringIO(source), htmlparser)
+    root = tree.getroot()
+    tr = root.xpath("//tr")
+    
+    def clean_join(*content):
+        return ' '.join([c.text.strip() for c in content])
 
-    # driver.quit()
-    # return source
+    for i in tr:
+        if len(i) >= 19:
+            # print(i[4].text, i[5].text, i[6].text)
+            # print(i[0][0].get("name"))
+            current_course = clean_join(i[4], i[5], i[6])
+            if current_course in courses:
 
-get_source(url)
+                checkbox = driver.find_element(By.NAME, i[0][0].get("name"))
+                driver.execute_script("arguments[0].click();", checkbox)
+                registered.append(current_course)
+                courses.remove(current_course)
+                if len(courses) == 0:
+                    break
 
-# with open('course-data.pickle', 'rb') as file:
-#     data = pickle.load(file)
+    submit = driver.find_element(By.NAME, 'submit')
+    driver.execute_script("arguments[0].click();", submit)
+    
+    # driver.implicitly_wait(3)
+    # WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'top_menu_strc')))
+    time.sleep(3)
+    submit2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, 'submit')))
+    driver.execute_script("arguments[0].click();", submit2)
+
+    try:
+        pending = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.NAME, 'ack')))
+    
+        while len(pending) != 0:
+            pending[0].click()
+            pending = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.NAME, 'ack')))
+    
+    except:
+        pass
 
 
-# for i in data:
-#     if 'CSCS 201' in i['name']:
-#         print(i['name'], i['seats'])
+courses = ['ENGL 110 A', 'EDUC 110 B']
+
+register(url, '202122', courses)
